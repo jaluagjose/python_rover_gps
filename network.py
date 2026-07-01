f526d0cde4ce3a2abd78b3149dc992b7254860ec
jaluagjose <jaluag_jose@yahoo.com.ph>
2026-07-01 22:29:23 +0800

    Milestone 2.5: Add TCP server for receiving GPS mission coordinates

--- network.py
+++ network.py
@@ -1,25 +1,53 @@
 import socket
 
-HOST = "0.0.0.0"
+HOST="0.0.0.0"
 PORT = 5000
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
 server.bind((HOST, PORT))
-
 server.listen(1)
 
-print("Waiting for mission...")
+print("-" * 50)
+print("Rover Mission Server")
+print("-" * 50)
+print(f"Listening on port {PORT}...")
+print()
 
 client, address = server.accept()
 
-print("Connected by:", address)
+print(f"Connected to {address}")
+print()
 
 while True:
 
 	data = client.recv(1024)
 
 	if not data:
+		print("Client disconnected.")
 		break
 
-	print("Received:", data.decode())
+	message = data.decode().strip()
+
+	print(f"Received: {message}")
+
+	try:
+
+		latitude, longitude = message.split(",")
+
+		latitude = float(latitude)
+
+		longitude = float(longitude)
+
+		print()
+		print("Mission Received")
+		print("---------------------")
+		print(f"Latitude : {latitude}")
+		print(f"Longitude: {longitude}")
+		print()
+
+	except Exception:
+
+		print("ERROR: Invalid GPS format")
+		print(":Expected:")
+		print("39.12345, -76.54321")
+		print()
